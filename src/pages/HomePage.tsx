6d0a32d6e56c7c9990c17f06da4b7848ec7700a9
Emir Aydın <84669393+ibrahimemiraydin@users.noreply.github.com>
2024-12-26 07:19:17 +0300

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -24,23 +24,23 @@ const HomePage: React.FC<HomePageProps> = ({ addTask, recentTasks }) => {
   return (
     <Sidebar>
       <div className="text-center bg-white rounded-lg p-8 shadow-lg">
-        <h1 className="text-4xl font-bold text-blue-600 mb-4">Welcome to the To-Do App</h1>
+        <h1 className="text-4xl font-bold text-blue-600 mb-4">Notefier</h1>
         <p className="text-gray-700 mb-8">Organize your tasks and boost your productivity!</p>
         <input
           type="text"
           value={taskTitle}
           onChange={(e) => setTaskTitle(e.target.value)}
           placeholder="New Task"
-          className="border p-2 mb-4 w-full"
+          className="border p-2 mb-4 w-3/4 mx-auto rounded"
         />
         <button
           onClick={handleAddTask}
-          className="bg-blue-500 hover:bg-blue-600 text-white rounded p-2 w-full mt-2 transition duration-300"
+          className="bg-blue-500 hover:bg-blue-600 text-white rounded-full p-4 w-3/4 mt-2 transition duration-300 shadow-lg transform hover:scale-105"
         >
           Add Task
         </button>
         <Link to="/tasks">
-          <button className="bg-blue-500 hover:bg-blue-600 text-white rounded-full p-4 px-8 transition duration-300 shadow-lg transform hover:scale-105 mt-4">
+          <button className="bg-blue-500 hover:bg-blue-600 text-white rounded-full p-4 w-3/4 mt-4 transition duration-300 shadow-lg transform hover:scale-105">
             Go to Task Manager
           </button>
         </Link>
